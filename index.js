5e23fd2b76281a8e37e383e6d1322b5c753c6029
gabriel alejandro mamani <gabriel370ac@gmail.com>
2023-05-05 20:23:27 -0300

    fix again

--- index.js
+++ index.js
@@ -4,16 +4,4 @@ console.log('helouda')
 function prueba(a) {
 
   const p = a === 2 ? "ok" : "bad"
-<<<<<<< HEAD
-}
-
-function hola() {
-  return "holllaaaaaaaas"
-}
-
-
-function jelouda() {
-  return 1 + 1
-=======
->>>>>>> parent of f8043e4 (add more js)
 }
